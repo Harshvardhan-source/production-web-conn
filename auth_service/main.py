6fa2a2f23bfef1cba6f789b855e2c86f1f941cec
Harshavardhan-source <ravindraacharya0512@gmail.com>
2026-04-16 00:23:36 +0530

    added new compo

--- auth_service/main.py
+++ auth_service/main.py
@@ -68,6 +68,7 @@ if not ALLOWED_ORIGINS:
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "https://production-web-conn.onrender.com"
     ]
 
 app.add_middleware(
