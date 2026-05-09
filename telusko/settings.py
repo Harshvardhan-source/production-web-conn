7d9b798d3ed8bf40d6bbaa97e3e69c4d4901a80c
Harshavardhan-source <ravindraacharya0512@gmail.com>
2026-05-09 16:36:15 +0530

    Updated

--- telusko/settings.py
+++ telusko/settings.py
@@ -51,6 +51,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:8000',
+    'https://frontend-production-web-ux7k.onrender.com',
   
 ]
 CSRF_COOKIE_SAMESITE = 'Lax'
